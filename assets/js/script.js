6e63771d70792406b0d23b2d15879e7269ab7269
Natalia Teaca <lilikoy@sbcglobal.net>
2020-06-18 15:09:27 -0700

    5.5.7

--- assets/js/script.js
+++ assets/js/script.js
@@ -182,21 +182,24 @@ $(".card .list-group").sortable({
     tolerance: "pointer",
     helper: "clone",
     activate: function(event) {
-        console.log("activate", this);
+        $(this).addClass("dropover");
+        $(".bottom-trash").addClass("bottom-trash-drag");
     },
     deactivate: function(event) {
-        console.log("deactivate", this);
+        $(this).removeClass("dropover");
+        $(".bottom-trash").removeClass("bottom-trash-drag");
     },
     over: function(event) {
-        console.log("over", event.target);
+        $(event.target).addClass("dropover-active");
+        $(".bottom-trash").addClass("bottom-trash-active");
     },
     out: function(event) {
-        console.log("out", event.target);
+        $(event.target).removeClass("dropover-active");
+        $(".bottom-trash").removeClass("bottom-trash-active");
     },
     update: function(event) {
         // array to store the task data in
         var tempArr = [];
-
         // loop over current set of children in sortable list
         $(this).children().each(function() {
             var text = $(this)
@@ -257,7 +260,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
     // get form values
     var taskText = $("#modalTaskDescription").val();
     var taskDate = $("#modalDueDate").val();
@@ -306,5 +309,10 @@ $("#modalDueDate").datepicker({
     // Pass task's <li> element into auditTask() to check new due date
     auditTask($(taskSpan).closest(".list-group-item"));
   });
+  setInterval(function () {
+    $(".card .list-group-item").each(function (el) {
+      auditTask(el);
+    });
+  }, (1000 * 60) * 30);
 // load tasks for the first time
 loadTasks();
